28dccab09a5ec535a8a35640b626471d552a4bff
Bimal Chaudhary <155341111+BimalChaudhary@users.noreply.github.com>
2025-01-29 18:37:13 +0545

    Add files via upload

---
+++ Chapter 1/modules.py
@@ -0,0 +1,16 @@
+import pyjokes
+
+# print ("Printing jokes....")
+# This print a random jokes
+
+joke  = pyjokes.get_joke()
+print(joke)
+
+"""
+so thgogjoajg ojgojgidfjgegbo
+gjosfjgojg
+hnsdgvodgo
+"""
+15151
+# gjijgjhg
+444
